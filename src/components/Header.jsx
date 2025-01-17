09aa8d75b0f453930ba0050ffeecc4b986b1e6c1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-17 23:56:29 +0200

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 export default function Header() {
+  const now = new Date();
   return (
     <header>
       <h3>Result</h3>
-      <span>There will be time here!</span>
+      <span>Time now: {now.toLocaleTimeString()}</span>
     </header>
   )
 }
