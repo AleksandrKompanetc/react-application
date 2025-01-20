5206808edd1d6d4d1490cc913f1b161a47419f21
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-20 23:13:08 +0200

    added states for component FeedbackSection

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import Button from "./Button/Button";
+
 export default function FeedbackSection() {
+  const [name, setName] = useState('');
+  const [reason, setReason] = useState(help);
+
   return (
     <section>
       <h3>Feedback</h3>
@@ -6,6 +12,15 @@ export default function FeedbackSection() {
       <form>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" className="control" />
+
+        <label htmlFor="reason">Reason for contact</label>
+        <select id="reason" className="control">
+          <option value="error">Error</option>
+          <option value="help">Need help</option>
+          <option value="suggest">Offer</option>
+        </select>
+
+        <Button>Send</Button>
       </form>
     </section>
   )
