37198b34994905082770b55a74bf563f594cdd9a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-20 22:49:53 +0200

    added form and styles

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -2,6 +2,11 @@ export default function FeedbackSection() {
   return (
     <section>
       <h3>Feedback</h3>
+
+      <form>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" className="control" />
+      </form>
     </section>
   )
 }
