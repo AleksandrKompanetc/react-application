73bada579a7f56c0dc2b7e0fd2f309ecfc926df1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-20 15:48:33 +0200

    changed displaying time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 
 export default function Header() {
-  const now = new Date();
+  const [now, setNow] = useState(new Date());
+  
+  setInterval(() => setNow(new Date()), 1000);
   return (
     <header>
       <h3>Result</h3>
