b929287ef520f9ebde6deecb00abf3523892ded8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-20 16:28:20 +0200

    changed classes for active button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,12 @@
 import './Button.css';
 
 export default function Button({children, onClick, isActive}) {
+  // let classes = 'button';
+  // if (isActive) classes += ' active';
 
-  return <button className={isActive ? 'button active' : 'button'} onClick={onClick}>{children}</button>
+  return (
+    <button className={isActive ? 'button active' : 'button'} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
