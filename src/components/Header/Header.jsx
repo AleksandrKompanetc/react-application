f2ed0e9b8483991d8672c8e120d4a1f2e4d09807
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-21 23:00:13 +0200

    added useEffect to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Header.css';
 
 export default function Header() {
   const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   
-  setInterval(() => setNow(new Date()), 1000);
   return (
     <header>
       <h3>Result</h3>
