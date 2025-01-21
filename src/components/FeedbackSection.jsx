ff84ec007cc37e215f2408b31e8c40721afb432a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-21 14:48:22 +0200

    added some changes to form

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -3,24 +3,52 @@ import Button from "./Button/Button";
 
 export default function FeedbackSection() {
   const [name, setName] = useState('');
-  const [reason, setReason] = useState(help);
+  const [hasError, setHasError] = useState(true);
+  const [reason, setReason] = useState('help');
+
+  function handleNameChange(event) {
+    setName(event.target.value)
+    setHasError(event.target.value.trim() === 0)
+  }
+
+  function toggleError() {
+    setHasError((prev) => !prev)
+    // setHasError(!hasError)
+  }
 
   return (
     <section>
       <h3>Feedback</h3>
 
+      <Button onClick={toggleError}>Toggle Error</Button>
+
       <form>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" className="control" />
+        <input 
+          type="text"  
+          id="name" 
+          className="control" 
+          value={name} 
+          style={{
+            border: hasError ? '1px solid red' : null,
+          }}
+          onChange={handleNameChange} 
+        />
 
         <label htmlFor="reason">Reason for contact</label>
-        <select id="reason" className="control">
+        <select id="reason" className="control" value={reason} onChange={(e) => setReason(e.target.value)}>
           <option value="error">Error</option>
           <option value="help">Need help</option>
-          <option value="suggest">Offer</option>
+          <option value="suggest">Suggest</option>
         </select>
 
-        <Button>Send</Button>
+        <pre>
+          Name: {name}
+          <br />
+          Reason: {reason}
+        </pre>
+
+        <Button disabled={hasError} isActive={!hasError} className='button'>Send</Button>
       </form>
     </section>
   )
