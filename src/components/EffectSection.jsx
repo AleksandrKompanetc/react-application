8a310216ab98df1103efbc1b0869e9ca245f4728
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-21 21:30:02 +0200

    added component Modal

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,7 +1,15 @@
+import Button from "./Button/Button";
+
 export default function EffectSection() {
+  function openModal() {
+
+  }
+  
   return (
     <section>
       <h3>Effects</h3>
+
+      <Button onClick={openModal}>Open Info</Button>
     </section>
   )
 }
