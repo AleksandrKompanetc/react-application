fadf3faa9127b12bb2cac70fa7a1737c3a8a8719
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-21 22:06:05 +0200

    added createPortal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,9 @@
+import { createPortal } from 'react-dom';
 import './Modal.css';
 
 export default function Modal({children, open}) {
-  return <dialog open={open}>{children}</dialog>
+  return createPortal(
+    <dialog open={open}>{children}</dialog>,
+    document.getElementById('modal')
+  )
 }
